e50c873007a6b8d177625acb274d11a2d8cb5daa
HSKKOU <hskk1120551@gmail.com>
2015-07-23 00:04:02 +0900

    Move function creating Postit

--- app/src/main/java/jp/ac/titech/itpro/sdl/tabra/Activity/BrainStorming/Main/PostitController.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/tabra/Activity/BrainStorming/Main/PostitController.java
@@ -3,10 +3,7 @@ package jp.ac.titech.itpro.sdl.tabra.Activity.BrainStorming.Main;
 import android.content.Context;
 import android.graphics.Point;
 import android.view.View;
-import android.widget.AbsoluteLayout;
-import android.widget.TextView;
 
-import jp.ac.titech.itpro.sdl.tabra.R;
 import jp.ac.titech.itpro.sdl.tabra.SQLite.Model.Item;
 
 /**
@@ -16,48 +13,25 @@ public class PostitController {
     private static final String TAG = PostitController.class.getSimpleName();
 
     private Context mContext;
-    private AbsoluteLayout mParentView;
     private PostitFactory mFactory;
 
-    private Point center = new Point(0,0);
+    private Point mCenter = new Point(0,0);
 
-    public PostitController(Context context, AbsoluteLayout parent) {
+    public PostitController(Context context) {
         this.mContext = context;
-        this.mParentView = parent;
         this.mFactory = new PostitFactory(mContext, this);
     }
 
-    public void createPostit(Item item) {
-        View postitview = mFactory.createPostitView();
-
-        TextView idText = (TextView)postitview.findViewById(R.id.postit_hidden_id);
-        idText.setText(item.getId() + "");
-
-        TextView contentText = (TextView)postitview.findViewById(R.id.postit_text);
-        contentText.setText(item.getContent() + "");
-
-        postitview.setBackgroundColor(colorStr2Int(mContext, item.getColor()));
-
-        postitview.setX(item.getPos_x());
-        postitview.setY(item.getPos_y());
-
-        mParentView.addView(postitview);
+    public View createPostit(Item item) {
+        return mFactory.createPostitView(item);
     }
 
     public Point getCenter() {
-        return this.center;
+        return this.mCenter;
     }
 
     public void setCenter(int x, int y) {
-        this.center.x = x;
-        this.center.y = y;
-    }
-
-    public static int colorStr2Int(Context context, String colorStr) {
-        if("postit_red".equals(colorStr)){return context.getResources().getColor(R.color.postit_red);}
-        else if("postit_yellow".equals(colorStr)){return context.getResources().getColor(R.color.postit_yellow);}
-        else if("postit_blue".equals(colorStr)){return context.getResources().getColor(R.color.postit_blue);}
-        else if("postit_green".equals(colorStr)){return context.getResources().getColor(R.color.postit_green);}
-        else{return context.getResources().getColor(R.color.postit_white);}
+        this.mCenter.x = x;
+        this.mCenter.y = y;
     }
 }
--- app/src/main/java/jp/ac/titech/itpro/sdl/tabra/Activity/BrainStorming/Main/PostitFactory.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/tabra/Activity/BrainStorming/Main/PostitFactory.java
@@ -5,8 +5,10 @@ import android.graphics.Point;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import jp.ac.titech.itpro.sdl.tabra.R;
+import jp.ac.titech.itpro.sdl.tabra.SQLite.Model.Item;
 
 /**
  * Created by hskk1120551 on 15/07/20.
@@ -27,13 +29,33 @@ public class PostitFactory {
         mInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    public View createPostitView() {
+    public View createPostitView(Item item) {
         View postit = mInflater.inflate(R.layout.postit, null);
         Point center = mPostitCtrl.getCenter();
         postit.setX(center.x - mPostitH/2);
         postit.setY(center.y - mPostitH / 2);
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(mPostitW, mPostitH);
         postit.setLayoutParams(layoutParams);
+
+        TextView idText = (TextView)postit.findViewById(R.id.postit_hidden_id);
+        idText.setText(item.getId() + "");
+
+        TextView contentText = (TextView)postit.findViewById(R.id.postit_text);
+        contentText.setText(item.getContent() + "");
+
+        postit.setBackgroundColor(colorStr2Int(mContext, item.getColor()));
+
+        postit.setX(item.getPos_x());
+        postit.setY(item.getPos_y());
+
         return postit;
+    }
+
+    public static int colorStr2Int(Context context, String colorStr) {
+        if("postit_red".equals(colorStr)){return context.getResources().getColor(R.color.postit_red);}
+        else if("postit_yellow".equals(colorStr)){return context.getResources().getColor(R.color.postit_yellow);}
+        else if("postit_blue".equals(colorStr)){return context.getResources().getColor(R.color.postit_blue);}
+        else if("postit_green".equals(colorStr)){return context.getResources().getColor(R.color.postit_green);}
+        else{return context.getResources().getColor(R.color.postit_white);}
     }
 }
