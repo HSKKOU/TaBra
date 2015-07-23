a5d2a08b847c5d04a3cdf1844edea63d31ec7d2c
HSKKOU <hskk1120551@gmail.com>
2015-07-24 08:59:21 +0900

    Add parameter Server_id

--- app/src/main/java/jp/ac/titech/itpro/sdl/tabra/SQLite/Controller/DBOpenHelper.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/tabra/SQLite/Controller/DBOpenHelper.java
@@ -12,7 +12,7 @@ public class DBOpenHelper extends SQLiteOpenHelper {
 
     // about DB
     public static final String DB_NAME = "TaBra";
-    public static final int DB_VERSION = 1;
+    public static final int DB_VERSION = 2;
 
     // TABLE name
     public static final String TABLE_THEMES = "themes";
@@ -24,9 +24,11 @@ public class DBOpenHelper extends SQLiteOpenHelper {
     public static final String KEY_CREATED_AT = "created_at";
     public static final String KEY_UPDATED_AT = "updated_at";
     public static final String KEY_USER_NAME = "user_name";
-    public static final String[] USER_COLUMS = {KEY_ID, KEY_USER_NAME, KEY_CREATED_AT, KEY_UPDATED_AT};
+    public static final String KEY_SERVER_ID = "server_id";
 
     // USER TABLE - column names
+    public static final String KEY_PASSWD = "passwd";
+    public static final String[] USER_COLUMS = {KEY_ID, KEY_USER_NAME, KEY_PASSWD, KEY_CREATED_AT, KEY_UPDATED_AT};
 
     // THEME TABLE - column names
     public static final String KEY_TITLE = "title";
@@ -39,13 +41,14 @@ public class DBOpenHelper extends SQLiteOpenHelper {
     public static final String KEY_COLOR = "color";
     public static final String KEY_POS_X = "position_x";
     public static final String KEY_POS_Y = "position_y";
-    public static final String[] ITEM_COLUMS = {KEY_ID, KEY_THEME_ID, KEY_CONTENT, KEY_USER_NAME, KEY_COLOR, KEY_POS_X, KEY_POS_Y, KEY_CREATED_AT, KEY_UPDATED_AT};
+    public static final String[] ITEM_COLUMS = {KEY_ID, KEY_THEME_ID, KEY_SERVER_ID, KEY_CONTENT, KEY_USER_NAME, KEY_COLOR, KEY_POS_X, KEY_POS_Y, KEY_CREATED_AT, KEY_UPDATED_AT};
 
     // Statements
     // USER TABLE create statement
     private static final String CREATE_TABLE_USER
             = "CREATE TABLE " + TABLE_USERS + "("
             + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+            + KEY_PASSWD + " TEXT NOT NULL,"
             + KEY_USER_NAME + " TEXT NOT NULL,"
             + KEY_CREATED_AT + " INTEGER NOT NULL,"
             + KEY_UPDATED_AT + " INTEGER NOT NULL"
@@ -66,6 +69,7 @@ public class DBOpenHelper extends SQLiteOpenHelper {
             = "CREATE TABLE " + TABLE_ITEMS + "("
             + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
             + KEY_THEME_ID + " INTEGER NOT NULL,"
+            + KEY_SERVER_ID + " INTEGER NOT NULL,"
             + KEY_CONTENT + " TEXT,"
             + KEY_USER_NAME + " TEXT NOT NULL,"
             + KEY_COLOR + " TEXT NOT NULL,"
